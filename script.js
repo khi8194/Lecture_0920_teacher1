706323533a228bd1a07b11209db9071137def625
tubi55 <tubi55@nate.com>
2024-09-20 11:07:55 +0900

    색상값 모션 적용

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 const btn = document.querySelector("button");
 const box = document.querySelector("article");
+const title = document.querySelector("h1");
 
-//btn click event
 btn.addEventListener("click", () => {
-  //call Anime
-  //new Anime(동작대상, {변경할css속성 프로퍼티}, {duration:모션시간})
-  //속성값 설정시 px단위는 숫자로만 입력
-  //퍼센트 단위는 문자열로 감싸서 입력
-  //모션시간인 duration은 숫자만입력 (ms단위) 디폴트값: 500
-  //opacity 제어 : 0~1사이의 실수값
-  //제어 가능 속성: width, height, opacity, top, left, bottom, left, color, backgroundColor, scroll
-  //margin, padding도 모션 처리 가능 (단 여백관련 속성은 %값 적용 불가, px단위로만 변경 가능)
-  //속성명은 camel case 규칙 적용하거나 아니면 ''로 감싸줌 (예: margin-left는 marginLeft 혹은 'margin-left'식으로 사용 )
-  new Anime(box, { "padding-left": 300 }, { duration: 1000 });
+  //색상 모션 처리
+  //색상값을 미리 지정된 키워드명이 아닌 16진수 표기법으로 변경해서 적용
+  new Anime(box, { backgroundColor: "#ff69b4" }, { duration: 1000 });
+
+  new Anime(title, { color: "#ff69b4" }, { duration: 2000 });
 });
