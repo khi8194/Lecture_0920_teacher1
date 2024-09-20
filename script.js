68c7ff3478e13bf698f97877f3e977ed8887bfa1
tubi55 <tubi55@nate.com>
2024-09-20 14:08:58 +0900

    forEach 기본 사용법 정리

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+/*
+  forEach
+  : 배열, 유사배열을 반복처리하기 위한 내장 함수
+
+  Array.forEach((data, idx)=>{
+    중괄호 안쪽의 코드블록은 배열의 갯수만큼 반복 처리됨
+    해당 코드블록 안쪽에서는 data, idx라는 파라미터로 전달되는 값 활용 가능
+    data: 현재 반복도는 배열의 데이터
+    idx: 현재 반복도는 순서값
+  })
+*/
 const btns = document.querySelectorAll("button");
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", (event) => {
-    //event객체 안쪽의 target property에는 이벤트가 발생한 대상 (DOM)정보가 담겨 있음
-    console.dir(event.target.dataset.pos);
-    //console.log(event.target.getAttribute("data-pos"));
-    //parsetInt(문자화된 숫자값) : 정수로 반환
-    //parseFloat(문자화된 숫자값) : 실수로 반환 (소숫점 아래까지 포함된 숫자)
-    new Anime(window, { scroll: parseInt(event.target.dataset.pos) });
-  });
+btns.forEach((btn, idx) => {
+  console.log(btn);
+  console.log(idx);
 });
