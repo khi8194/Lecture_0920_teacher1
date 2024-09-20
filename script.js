c3a2eb44b9c355bbfd3d0308e97f03884cca69bc
tubi55 <tubi55@nate.com>
2024-09-20 10:42:31 +0900

    모션 처리 가능한 스타일 property 정리

--- script.js
+++ script.js
@@ -8,5 +8,7 @@ btn.addEventListener("click", () => {
   //속성값 설정시 px단위는 숫자로만 입력
   //퍼센트 단위는 문자열로 감싸서 입력
   //모션시간인 duration은 숫자만입력 (ms단위) 디폴트값: 500
-  new Anime(box, { top: "50%", left: "50%" }, { duration: 1000 });
+  //opacity 제어 : 0~1사이의 실수값
+  //제어 가능 속성: width, height, opacity, top, left, bottom, left, color, backgroundColor, scroll
+  new Anime(box, { width: 300, left: "50%", opacity: 0 }, { duration: 1000 });
 });
