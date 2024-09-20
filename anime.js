48010e481e034dd9e296d2cbfd2a027a6c3ec442
tubi55 <tubi55@nate.com>
2024-09-20 11:51:39 +0900

    scroll 코드 수정

--- anime.js
+++ anime.js
@@ -22,10 +22,13 @@ class Anime {
 
   getValue(key, value, type) {
     let currentValue = null;
-    currentValue = parseFloat(getComputedStyle(this.selector)[key]);
+
+    //currentValue = parseFloat(getComputedStyle(this.selector)[key]);
+    currentValue = this.selector.scrollY;
     key === "scroll"
       ? (currentValue = this.selector.scrollY)
-      : (currentValue = parseFloat(getComputedStyle(this.selector)[key]));
+      : //: (currentValue = parseFloat(getComputedStyle(this.selector)[key]));
+        (currentValue = this.selector.scrollY);
 
     if (type === "percent") {
       const parentW = parseInt(
